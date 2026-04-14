1ba91a3c452879c419ac365017c184712ade0fff
Deemah <deemah.spw@gmail.com>
2026-04-14 18:47:39 +0300

    Tractor_Path completed

---
+++ Assets/Scripts/WheelTerrainPainter.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class WheelTerrainPainter : MonoBehaviour
+{
+    public Terrain terrain;
+    public int textureIndex = 1; // индекс текстуры грязи в Terrain Layer
+    public float brushSize = 1.0f;
+    public float brushStrength = 0.5f;
+
+    private TerrainData terrainData;
+
+    void Start()
+    {
+        if (terrain == null)
+            terrain = Terrain.activeTerrain;
+
+        terrainData = terrain.terrainData;
+    }
+
+    void Update()
+    {
+        PaintUnderWheel();
+    }
+
+    void PaintUnderWheel()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 2f))
+        {
+            Vector3 terrainPos = hit.point - terrain.transform.position;
+
+            int mapX = (int)((terrainPos.x / terrainData.size.x) * terrainData.alphamapWidth);
+            int mapZ = (int)((terrainPos.z / terrainData.size.z) * terrainData.alphamapHeight);
+
+            int size = Mathf.RoundToInt(brushSize);
+
+            float[,,] splat = terrainData.GetAlphamaps(mapX, mapZ, size, size);
+
+            for (int x = 0; x < size; x++)
+            {
+                for (int z = 0; z < size; z++)
+                {
+                    // уменьшаем траву
+                    splat[x, z, 0] *= (1f - brushStrength);
+
+                    // добавляем грязь
+                    splat[x, z, textureIndex] += brushStrength;
+                }
+            }
+
+            terrainData.SetAlphamaps(mapX, mapZ, splat);
+        }
+    }
+}
